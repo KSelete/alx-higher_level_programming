4cf037d548f15005d8fd677e7781782d5b24babd
KSelete <keketsoselete@gmail.com>
2024-03-21 14:50:03 -0700

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,29 +1,16 @@
 #!/usr/bin/python3
-def _reduce(fn, seq, init=0):
-    """ Reduce a list to a single value """
-    try:
-        return _reduce(fn, seq[1:], fn(init, seq[0]))
-    except IndexError:
-        return init
-
 def roman_to_int(roman_string):
-    """ Convert a Roman numeral to an integer """
-    n2n = {
-            'I': 1,
-            'V': 5,
-            'X': 10,
-            'L': 50,
-            'C': 100,
-            'D': 500,
-            }
-    try:
-        numbers = list(map(n2n.get, roman_string[::-1]))
-        integer = numbers[0]
-    except (IndexError, KeyError, TypeError):
+    if not isinstance(roman_string, str) or roman_string is None:
         return 0
-    for i in range(len(numbers[1:])):
-        if numbers[i + 1] >= numbers[i]:
-            integer += numbers[i + 1]
+
+    roman_values = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                    'C': 100, 'D': 500, 'M': 1000}
+    total = 0
+
+    for i in range(len(roman_string)):
+        if i < len(roman_string) - 1 and roman_values[roman_string[i]] < roman_values[roman_string[i + 1]]:
+            total -= roman_values[roman_string[i]]
         else:
-            integer -= numbers[i + 1]
-            return integer
+            total += roman_values[roman_string[i]]
+
+    return total
